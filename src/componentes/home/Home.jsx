f4db89c87170cb37d3b81ca8e45d1eb7111fc67b
PaosLuna <lunapp20@gmail.com>
2023-09-06 01:34:32 -0600

    Feat reac router

---
+++ src/componentes/home/Home.jsx
@@ -0,0 +1,39 @@
+import { useFetch } from "../../servicios/useFetch";
+
+const Home = () => {
+  const imageUrl = "https://image.tmdb.org/t/p/original";
+
+  const { data } = useFetch(
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=es-ES&page=1&sort_by=popularity.desc"
+  );
+
+  /* const { data } = useFetch("https://probando-errores.com"); */
+
+  const resultado = data.results;
+  console.log(resultado);
+
+  return (
+    <div>
+      <h1>peliculas</h1>
+      <div>
+        {data.results ? (
+          data.results.map((movie) => (
+            <div key={movie.id}>
+              <img
+                src={`${imageUrl + movie.poster_path}`}
+                alt={movie.title}
+                width={300}
+                height={400}
+              />
+              <h1>{movie.title}</h1>
+            </div>
+          ))
+        ) : (
+          <p>No se encuentran las películas</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
