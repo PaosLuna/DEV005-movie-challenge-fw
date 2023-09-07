a9429c383a8587de757a2f58eda22bdb13581c90
PaosLuna <lunapp20@gmail.com>
2023-09-06 18:38:00 -0600

    Feat detectar genero de peliculas

--- src/componentes/Generos.jsx
+++ src/componentes/Generos.jsx
@@ -2,16 +2,56 @@ import { useGenres } from "../servicios/useGeneros";
 import { useFetch } from "../servicios/useFetch";
 import { useEffect } from "react";
 
-const CompararGeneros = () => {
+/* const CompararGeneros = () => {
+  const { data } = useFetch(
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=es-ES&page=1&sort_by=popularity.desc"
+  );
+
   const generos = useGenres();
   const generosMap = generos.data.genres;
 
   for (const key in generosMap) {
     const genero = generosMap[key];
-    console.log(`ID: ${genero.id}, NAME: ${genero.name}`);
+    const genderName = `ID: ${genero.id}, NAME: ${genero.name}`;
+    return genderName;
   }
 
-  console.log(generosMap, 16);
+  if (data.results) {
+    let genreIdsArray = data.results.map((pelicula) => pelicula.genre_ids);
+    console.log(genreIdsArray);
+  }
+
+  //console.log(generosMap, 16);
+}; */
+
+const CompararGeneros = () => {
+  const { data } = useFetch(
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=es-ES&page=1&sort_by=popularity.desc"
+  );
+
+  const generos = useGenres();
+  const generosMap = generos.data.genres;
+  const movieGenero = [];
+
+  const generosIdNombre = {};
+  for (const key in generosMap) {
+    const genero = generosMap[key];
+    generosIdNombre[genero.id] = genero.name;
+  }
+
+  if (data.results) {
+    const genreIdsArray = data.results.map((pelicula) => pelicula.genre_ids);
+
+    // Compara los genre_ids de las películas con los ids de género y muestra los nombres de género correspondientes
+    genreIdsArray.forEach((genreIds) => {
+      const nombresGeneros = genreIds.map(
+        (genreId) => generosIdNombre[genreId]
+      );
+      movieGenero.push(nombresGeneros);
+      //console.log("Nombres de género de la película:", nombresGeneros);
+    });
+  }
+  return movieGenero;
 };
 
 export default CompararGeneros;
