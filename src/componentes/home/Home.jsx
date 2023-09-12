cb175a64c080c7582a35af2de3dab22f7a92c1b5
PaosLuna <lunapp20@gmail.com>
2023-09-11 21:46:44 -0600

    todo bien aqui

--- src/componentes/home/Home.jsx
+++ src/componentes/home/Home.jsx
@@ -4,6 +4,7 @@ import { useFetch } from "../../servicios/useFetch";
 import { useGenres } from "../../servicios/useGeneros";
 import SelectGenero from "../selects/SelectGenero";
 import { MostarPeliculasContext } from "../context/Context";
+import Banner from "../slider/Slider";
 
 const Home = () => {
   const imageUrl = "https://image.tmdb.org/t/p/original";
@@ -11,6 +12,8 @@ const Home = () => {
   const [orderAbc, setOrderAbc] = useState(null);
   /*   const [select, setSelect] = useState(null); */
   const [filtroGeneros, setFiltroGeneros] = useState(false);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+
   const context = useContext(MostarPeliculasContext);
 
   const { data } = useFetch(
@@ -24,6 +27,7 @@ const Home = () => {
     dataMovies = data.results.slice();
   }
   console.log(generos, 24);
+
   const handleOrderChange = (e) => {
     const selectedOrder = e.target.value;
 
@@ -48,26 +52,38 @@ const Home = () => {
 
   return (
     <div>
-      <h1 className="bg-pink-500">peliculas</h1>
-      <select onChange={handleOrderChange}>
-        <option disabled selected>
-          A-Z
-        </option>
-        <option value={"ASCENDENTE"}>ASCENDENTE</option>
-        <option value={"DESCENDENTE"}>DESCENDENTE</option>
-      </select>
-      <SelectGenero handleFiltroGeneros={handleFiltroGeneros} />
+      <Banner />
+      <div className="flex gap-5 ml-2 pb-4 w-100 h-14">
+        <select
+          onChange={handleOrderChange}
+          className="bg-purple-500 text-white text-xl text-center rounded-md w-40 h-10 cursor-pointer"
+        >
+          <option disabled selected>
+            A-Z
+          </option>
+          <option value={"ASCENDENTE"}>ASCENDENTE</option>
+          <option value={"DESCENDENTE"}>DESCENDENTE</option>
+        </select>
+        <SelectGenero
+          handleFiltroGeneros={handleFiltroGeneros}
+          filteredMovies={filteredMovies}
+          setFilteredMovies={setFilteredMovies}
+          handleGeneroChange={handleGeneroChange}
+        />
+      </div>
 
-      <div>
+      <div className="flex flex-wrap">
         {data.results && !filtroGeneros ? (
           orderMovies.map((movie) => (
-            <div key={movie.id}>
-              <img
-                src={`${imageUrl + movie.poster_path}`}
-                alt={movie.title}
-                width={300}
-                height={400}
-              />
+            <div key={movie.id} className="px-2 w-1/4 pb-4">
+              <div className="mb-4">
+                <img
+                  src={`${imageUrl + movie.poster_path}`}
+                  alt={movie.title}
+                  width={300}
+                  height={400}
+                />
+              </div>
               <h1>{movie.title}</h1>
               <p>Género: {generos[movie.genre_ids[0]]}</p>
               <p>Calificación: {movie.vote_average}⭐</p>
